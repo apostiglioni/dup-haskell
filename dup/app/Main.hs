80666f5d5ef42ef30caa407c93d8bbcf6a6023ce
Andres Postiglioni <andres.postiglioni@wsp344919wss.wdw.disney.com>
2016-05-26 14:33:40 -0400

    Cleanup
    Move things around in the file
    added data types in function definitions

--- dup/app/Main.hs
+++ dup/app/Main.hs
@@ -1,14 +1,9 @@
--- walk2 "test" $= prune $= mapSummary $= gSize $= (dig md5) $= (digContent cmpFilesData)$$ CL.consume
-
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE LambdaCase #-}
 
 module Main where
 
 import Lib
---import Pipes
---import qualified Pipes.Prelude as PP
---import qualified Pipes.ByteString as PB
 import Control.Monad (forM_, unless, when, liftM2, liftM, (<=<))
 import System.Directory (doesDirectoryExist, getDirectoryContents, canonicalizePath)
 import System.Environment (getArgs)
@@ -28,6 +23,7 @@ import Data.Function (fix)
 -- type Sink a m b = ConduitM a Void m b -- no meaningful output value
 
 
+
 import Data.Conduit
 import Data.List (mapAccumL)
 
@@ -37,9 +33,34 @@ import Text.Printf (printf)
 
 import Data.ByteString.Lazy (hGet, readFile)
 
+data FileSummary = FileSummary {
+                  path   :: FilePath
+                , status :: FileStatus
+                }
+instance Show FileSummary where
+  --show fileData = "{ path='" ++ (path fileData) ++ "' }"
+  show fileData = printf "{ path='%s' }" (path fileData)
 
---walk :: FilePath -> Source IO FilePath
---walk path = sourceDirectoryDeep False
+
+data Partitioner = FileSize FileOffset --FileSize
+                 | D (Digest MD5) -- TODO: There must be a way to generalize MD5
+                 | Content
+                 deriving (Show, Ord, Eq)
+
+
+data Cluster a b = Cluster {
+                     key   :: [a]
+                   , value :: [b]
+                   }
+                   deriving (Show)
+
+
+type ClusterKey = [Partitioner]
+
+type FileSummaryCluster = Cluster Partitioner FileSummary
+
+-- walk :: FilePath -> Source IO FilePath
+-- walk path = sourceDirectoryDeep False
 
 
 walk2 :: FilePath -> Source IO FilePath
@@ -53,30 +74,8 @@ walk2 topPath = do
       then walk2 path
       else yield path
 
-data FileSummary = FileSummary {
-                  path   :: FilePath
-                , status :: FileStatus
-                }
 
-instance Show FileSummary where
-  --show fileData = "{ path='" ++ (path fileData) ++ "' }"
-  show fileData = printf "{ path='%s' }" (path fileData)
-
-data Partitioner = FileSize FileOffset --FileSize
-                 | D (Digest MD5) -- TODO: There must be a way to generalize MD5
-                 | Content
-    deriving (Show, Ord, Eq)
-
-type ClusterKey = [Partitioner]
-
-data Cluster a b = Cluster {
-                        key   :: [a]
-                      , value :: [b]
-                      }
-    deriving (Show)
-
-
-dig :: (FileSummary -> IO Partitioner) -> ConduitM (Cluster Partitioner FileSummary) (Cluster Partitioner FileSummary) IO ()
+dig :: (FileSummary -> IO Partitioner) -> Conduit FileSummaryCluster IO FileSummaryCluster
 dig classifier =
   await >>= \case
     Nothing -> return ()
@@ -87,11 +86,11 @@ dig classifier =
           yield $ Cluster clusterKey clusterValue
           dig classifier
         else do
-          (`Map.traverseWithKey` categories) $ \key -> 
+          (`Map.traverseWithKey` categories) $ \key ->
             yield . Cluster (key : clusterKey)
           dig classifier
 
-
+digContent :: (FileSummary -> FileSummary -> IO Bool) -> Conduit FileSummaryCluster IO FileSummaryCluster
 digContent classifier =
   await >>= \case
     Nothing -> return ()
@@ -160,9 +159,6 @@ classify classifier = loop Map.empty
       loop (Map.insert category (x : members) acc) xs
 
 
-type FileSummaryCluster = Cluster Partitioner FileSummary
-
-
 mapSummary :: Conduit FilePath IO FileSummary
 -- ^Maps a stream of FilePath to a stream of FileSummary
 -- awaitForEver $ \path -> do
@@ -177,19 +173,17 @@ gSize :: Conduit FileSummary IO FileSummaryCluster
 gSize = conduit Map.empty
   where
     conduit :: Map.Map FileOffset FileSummaryCluster -> Conduit FileSummary IO FileSummaryCluster
-    conduit map = do
-      maybeData <- await
-      case maybeData of
-        Just fileData -> do
-          let size = fileSize (status fileData)
-          let maybeDups = Map.lookup size map
-          case maybeDups of
+    conduit map =
+      await >>= \case
+        Just fileData ->
+          let size = fileSize (status fileData) in
+          case (Map.lookup size map) of
             -- In both cases I'm doing conduit $ Map.insert size <something>
             -- there must be a way to abstract that
             -- Map.insert size (fromMaybe (...) (...))
             Nothing -> conduit $ Map.insert size (Cluster [FileSize size] [fileData]) map
-            Just cluster -> do
-              let newCluster = Cluster (key cluster) (fileData : (value cluster))
+            Just cluster ->
+              let newCluster = Cluster (key cluster) (fileData : (value cluster)) in
               conduit $ Map.insert size newCluster map
         Nothing -> do
           traverse yield map
@@ -204,7 +198,7 @@ prune = conduit Set.empty
   -- parametro mas para mantener el estado???
   where
     conduit :: Set.Set FilePath -> Conduit FilePath IO FilePath
-    conduit set = 
+    conduit set =
       await >>= \case
         Nothing -> return ()
         Just path -> do
@@ -216,52 +210,13 @@ prune = conduit Set.empty
               conduit $ Set.insert canonicalPath set
 
 
-sinkPutStrLn :: Sink String IO () -- consumes a stream of Strings, no result
-sinkPutStrLn = CL.mapM_ putStrLn
-
---TODO Refactor hashPipe
---sink :: Sink String IO ()
---sink = do
---    mstr <- await
---    case mstr of
---        Nothing -> return ()
---        Just str -> do
---            liftIO $ putStrLn str
---            sink
---sink = awaitForever $ liftIO . putStrLn
-
---sizePipe :: Conduit FilePath IO Map
---
-
--- hashPipe :: Conduit FilePath IO (FileHash MD5)
--- hashPipe = do
---   maybePath <- await
---   case maybePath of
---     Nothing -> return ()
---     Just path -> do
---       md5 <- hashFile path
---       yield $ FileHash path md5
---       hashPipe
-
-main :: IO ()
-main = do
-  [path] <- getArgs
-
-  md5 <- hashFile "dup.cabal" :: IO (Digest MD5)
-  print $ "dup.cabal " ++ show md5
-
---  hashFile "dup.cabal" >> print
-----  Couldn't match expected type ‘IO ()’ with actual type ‘a0 -> IO ()’
-----  Probable cause: ‘print’ is applied to too few arguments
-----  In the second argument of ‘(>>)’, namely ‘print’
-----  In a stmt of a 'do' block: hashFile "dup.cabal" >> print
-
-
-
 md5 :: FileSummary -> IO Partitioner
 -- TODO: Por que son iguales? Por que en uno return y en otro fmap
 --  md5 p = do
 --    hash <- hashFile $ path p
 --    return $ D hash
 md5 = fmap D . hashFile . path
-  
+
+
+-- main :: IO ()
+-- main = walk2 "test" $= prune $= mapSummary $= gSize $= (dig md5) $= (digContent cmpFilesData)$$ CL.consume
